aa38365a1d62dd430c104e9c62f455b6e09f4d1d
haru <harujisaku@gmail.com>
2020-06-23 14:35:24 +0900

    first commit

---
+++ dotfilesLink.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.profile ~/.profile
+ln -sf ~/dotfiles/.tmux_local.conf ~/.tmux_local.conf
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.vim/ ~/.vim
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.viminfo ~/.viminfo
+ln -sf ~/dotfiles/.tmux ~/.tmux
