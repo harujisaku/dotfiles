866a3f61016c2b4e77c5f10a729d524e6891c555
harujisaku <harujisaku@gmail.com>
2020-06-24 21:17:43 +0900

    message

--- .vimrc
+++ .vimrc
@@ -21,3 +21,56 @@ endif
 
 filetype plugin indent on
 syntax enable
+
+"文字コードをutf-8に変更
+set fenc=utf-8
+"行番号表示
+set number
+"現在の行を強調(横)
+set cursorline
+"行末の一文字先までカーソル移動可に
+set virtualedit=onemore
+"スマートインデント(謎)
+set smartindent
+"ビープ音を可視化
+set visualbell
+"括弧入力時の対括弧可視化
+set showmatch
+"ステータスラインを表示
+set laststatus=2
+"コマンドの保管
+set wildmode=list:longest
+"折り返し時の移動可に
+nnoremap j gj
+nnoremap k gk
+"検索語をハイライト表示
+set hlsearch
+"escでハイライト解除
+nmap <Esc><Esc> :nohlsearch<CR><Esc>
+
+noremap <Up> <Nop>
+noremap <Down> <Nop>
+noremap <Left> <Nop>
+noremap <Right> <Nop>
+inoremap <Up> <Nop>
+inoremap <Down> <Nop>
+inoremap <Left> <Nop>
+inoremap <Right> <Nop>
+
+set wildmenu
+set guioptions+=m
+set title
+set mouse=a
+set expandtab
+set autoindent
+set showcmd
+set ruler
+inoremap <C-j> <Down>
+inoremap <C-k> <Up>
+inoremap <C-h> <Left>
+inoremap <C-l> <Right>
+inoremap <silent> jj <Esc>
+inoremap <silent> っj <ESC>
+filetype plugin indent on
+set tabstop=4
+set shiftwidth=4
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,43 @@
 #!bin/bash
+#各ソフトがインストールされていなければインストール
+if !(type "vim" > /dev/null 2>&1); then
+	sudo apt install vim -y
+fi
+if !(type "tmux" > /dev/null 2>&1); then
+	sudo apt install tmux -y
+	sudo apt install tmuxp -y
+#nerd-fontsをインストールされていてもインストールしちゃう
+	mkdir nerd-fonts
+	cd nerd-fonts
+	git clone https://github.com/ryanoasis/nerd-fonts.git
+	sudo ./install.sh
+	cd ~/
+fi
+if !(type "gotop" > /dev/null 2>&1); then
+	if !(type "go" > /dev/null 2>&1); then
+		sudo apt install golang-go -y
+	fi
+	go get github.com/cjbassi/gotop
+fi
+if !(type "ranger" > /dev/null 2>&1); then
+	sudo apt install ranger -y
+fi
+if !(type "w3m" > /dev/null 2>&1); then
+	sudo apt install w3m w3m-img -y
+fi
+if !(type "xfce4-terminal" > /dev/null 2>&1); then
+	sudo apt install xfce4-terminal -y
+fi
+if !(type "chromium-browser" > /dev/null 2>&1); then
+	sudo apt install chromium-browser -y
+fi
+if !(type "ibus" > /dev/null 2>&1); then
+	sudo apt install ibus-mozc -y
+fi
+if !(type "jq" > /dev/null 2>&1); then
+    sudo apt install jq -y
+fi
+#各ドットファイルをシンボリックリンクで結ぶ
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.profile ~/.profile
 ln -sf ~/dotfiles/.tmux_local.conf ~/.tmux_local.conf
